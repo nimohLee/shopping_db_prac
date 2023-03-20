5513a18aa1081f1792a3ae8be1ec19664409d058
nimoh <spakers38@gmail.com>
2023-03-20 13:44:52 +0900

    First commit

---
+++ src/test/java/mall/shopping/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package mall.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
